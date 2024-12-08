1d68376d05b34c5122c20c85a846f03104cce9d2
Fernando de Souza <fernando_dsouza@icloud.com>
2024-12-08 16:59:11 -0300

    Adiciona a secão de SOBRE e o começo da seção de SERVIÇOS, com melhorias e correções no código e na estilização

--- assets/JS/hamburger.js
+++ assets/JS/hamburger.js
@@ -1,14 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
-const icons = document.querySelector('.icons');
+const menuLinks = document.querySelectorAll('.nav-list a');
 
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');  // Alterna a classe 'active' ao clicar no hamburger
-    
-    // Alterna a visibilidade dos ícones
-    if (nav.classList.contains('active')) {
-        icons.style.display = 'none';
-    } else {
-        icons.style.display = 'flex';
-    }
+    nav.classList.toggle('active')
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
 });
